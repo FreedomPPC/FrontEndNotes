9f3dc3c22e6c2d2a181cc61ddf9530cfeed349b9
yunhao <yunhao.li@sunlight-tech.com>
2017-01-12 18:57:57 +0800

    搭建vue环境

---
+++ MyDemo/00.vueJS/02.project/src/App.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <h1>
+  	welcome vue world111!
+  </h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ MyDemo/00.vueJS/02.vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <h1 class="">
+      welcome to sun-light technology
+  </h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+    h1 {
+        color: red;
+    }
+</style>
