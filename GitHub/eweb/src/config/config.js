7c04659422c76d979507e1a75124f07761541a47
yunhao <pBubbleme@163.com>
2017-02-07 23:08:54 +0800

    rename

--- GitHub/eweb/src/config/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-    serverUrl:process.env.NODE_ENV == 'production'? "":"/api/"//"http://static.jzez100.com/api"
-
-}
--- GitHub/eweb/src/stores/getters.js
+++
@@ -1,2 +0,0 @@
-export const getTest = ({commonStore}) => commonStore.text;
-export const getTitle= ({commonStore}) => commonStore.titleString;
