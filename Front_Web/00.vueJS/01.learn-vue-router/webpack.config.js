b1f062290e36c450b0104cd22bcd7e038da9b670
LoveLime <PBubbleMe@163.com>
2017-02-14 18:45:04 +0800

    vue-router

--- Front_Web/00.vueJS/01.learn-vue-router/webpack.config.js
+++ Front_Web/00.vueJS/01.learn-vue-router/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     //配置本地服务器webpack-dev-server
     devServer:{
         //本地服务器所加载的页面所在的目录,一般指向根目录下的index.html文件,这里把index.html放在public下;
-        // contentBase:'./public',
+        contentBase:'./public',
         colors:true,//终端中输出结果为彩色,
         historyApiFallback:true,//不跳转
         inline:true//实时刷新
