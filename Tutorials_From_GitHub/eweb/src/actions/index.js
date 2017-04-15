14a03becfb93d68da22455791a223f2cd14d0529
yunhao.li <pBubbleMe@163.com>
2017-04-15 18:26:32 +0800

    remove github tutorials

--- Tutorials_From_GitHub/eweb/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import * as types from 'stores/mutation-types'
-
-export const textChange = ({ dispatch, state }, text) => {
-    if(text) dispatch(types.TEST, text)
-}
-
-export const setTitle = ({ dispatch, state }, str) => {
-    if(str) dispatch(types.TITLE, str)
-}
--- Tutorials_From_GitHub/eweb/src/common/mixinPage.js
+++
@@ -1,9 +0,0 @@
-
-let pageBase = {
-        attached: function(){
-            //处理复杂逻辑
-            if(this.dealFun) setTimeout(()=>{this.dealFun();});
-        }
-   }
-
-export default pageBase
