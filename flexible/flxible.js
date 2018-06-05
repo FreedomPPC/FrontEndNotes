e496d952e6a40a4f528e67dd68ff0db32dfe4ac0
Rick_li <pBubbleMe@163.com>
2018-06-05 20:40:35 +0800

    flexible

---
+++ flexible/flxible.js
@@ -0,0 +1,136 @@
+(function(win, lib) {
+    const doc = win.document;
+    const docEl = doc.documentElement;
+    let metaEl = doc.querySelector('meta[name="viewport"]');
+    const flexibleEl = doc.querySelector('meta[name="flexible"]');
+    let dpr = 0;
+    let scale = 0;
+    let tid;
+    const flexible = lib.flexible || (lib.flexible = {});
+    let device = 'other';
+
+    if (metaEl) {
+        const match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
+
+        if (match) {
+            scale = parseFloat(match[1]);
+            dpr = parseInt(1 / scale);
+        }
+    } else if (flexibleEl) {
+        const content = flexibleEl.getAttribute('content');
+
+        if (content) {
+            const initialDpr = content.match(/initial\-dpr=([\d\.]+)/);
+            const maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/);
+
+            if (initialDpr) {
+                dpr = parseFloat(initialDpr[1]);
+                scale = parseFloat((1 / dpr).toFixed(2));
+            }
+            if (maximumDpr) {
+                dpr = parseFloat(maximumDpr[1]);
+                scale = parseFloat((1 / dpr).toFixed(2));
+            }
+        }
+    }
+    if (!dpr && !scale) {
+        const isAndroid = win.navigator.appVersion.match(/android/gi);
+        const isIPhone = win.navigator.appVersion.match(/iphone/gi);
+        const devicePixelRatio = win.devicePixelRatio;
+        var dpr_true = 1;
+
+        if (isIPhone) {
+            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
+                dpr = 3;
+            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
+                dpr = 2;
+            } else {
+                dpr = 1;
+            }
+            device = 'device-ios';
+            dpr_true = dpr;
+        } else {
+            dpr = 1;
+            device = 'device-android';
+            dpr_true = parseInt(devicePixelRatio) || 1;
+            dpr_true = dpr_true > 3 ? 3 : dpr_true;
+        }
+        scale = 1 / dpr;
+    }
+    docEl.setAttribute('data-dpr', dpr);
+    docEl.setAttribute('data-dpro', dpr_true);
+    if (!metaEl) {
+        metaEl = doc.createElement('meta');
+        metaEl.setAttribute('name', 'viewport');
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+        if (docEl.firstElementChild) {
+            docEl.firstElementChild.appendChild(metaEl);
+        } else {
+            const wrap = doc.createElement('div');
+
+            wrap.appendChild(metaEl);
+            doc.write(wrap.innerHTML);
+        }
+    }
+    function refreshRem() {
+        let width = docEl.getBoundingClientRect().width;
+
+        if (width / dpr > 750) {
+            width = 750 * dpr;
+        }
+        // if (width / dpr < 375) {
+        //    width = 375 * dpr;
+        // }
+        const rem = width / 10;
+
+        docEl.style.fontSize = rem + 'px';
+        flexible.rem = win.rem = rem;
+    }
+    win.addEventListener('resize',
+        ()=> {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 300);
+        },
+        false);
+    win.addEventListener('pageshow',
+        e=> {
+            if (e.persisted) {
+                clearTimeout(tid);
+                tid = setTimeout(refreshRem, 300);
+            }
+        },
+        false);
+
+    /*
+    if (doc.readyState === 'complete') {
+      //  doc.body.style.fontSize = 12 * dpr + 'px';
+        doc.body.className += ' ' + device;
+    } else {
+        doc.addEventListener('DOMContentLoaded',
+            function(e) {
+              //  doc.body.style.fontSize = 12 * dpr + 'px';
+                doc.body.className += ' ' + device;
+            },
+            false);
+    } */
+
+    refreshRem();
+    flexible.dpr = win.dpr = dpr;
+    flexible.refreshRem = refreshRem;
+    flexible.rem2px = function(d) {
+        let val = parseFloat(d) * this.rem;
+
+        if (typeof d === 'string' && d.match(/rem$/)) {
+            val += 'px';
+        }
+        return val;
+    };
+    flexible.px2rem = function(d) {
+        let val = parseFloat(d) / this.rem;
+
+        if (typeof d === 'string' && d.match(/px$/)) {
+            val += 'rem';
+        }
+        return val;
+    };
+}(window, window['lib'] || (window['lib'] = {})));
